e68a4246bcedcb7bdc159494d84eb7b8f2ba25b8
sirline <m_sireline@hotmail.com>
2018-10-05 02:28:36 +0900

    Added: step5

---
+++ step5/index.py
@@ -0,0 +1,56 @@
+from flask import Flask
+from flask import url_for
+from flask import redirect
+from flask import render_template
+from flask import request
+
+from DataStore.MySQL import MySQL
+dns = {
+    'user': 'mysql',
+    'host': 'localhost',
+    'password': 'NewPassword',
+    'database': 'kaggle'
+}
+db = MySQL(**dns)
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    props = {'title': 'Step-by-Step Flask - index', 'msg': 'Welcom to Index Page.'}
+    html = render_template('index.html', props=props)
+    return html
+
+@app.route('/hello')
+def hello():
+    props = {'title': 'Step-by-Step Flask - hello', 'msg': 'Hello World.'}
+    html = render_template('hello.html', props=props)
+    return html
+
+@app.route('/users', methods=['GET'])
+def users():
+    props = {'title': 'Users List', 'msg': 'Users List'}
+    stmt = 'SELECT * FROM users'
+    users = db.query(stmt)
+    html = render_template('users.html', props=props, users=users)
+    return html
+
+@app.route('/users/<int:id>', methods=['GET','PUT','DELETE'])
+def user(id):
+    props = {'title': 'User Information', 'msg': 'User Information'}
+    if request.method == 'DELETE':
+        stmt = 'DELETE FROM users WHERE id = ?'
+    elif request.method == 'PUT':
+        stmt = 'UPDATE users SET name=?, age=?, sex=? WHERE id = ?'
+    else:
+        stmt = 'SELECT * FROM users WHERE id = ?'
+    user = db.query(stmt, id, prepared=True)
+    html = render_template('user.html', props=props,user=user[0])
+    return html
+
+@app.errorhandler(404)
+def not_found(error):
+    return redirect(url_for('main'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
